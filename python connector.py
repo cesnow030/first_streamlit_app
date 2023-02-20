195a80ef53526644d2199fdcd68a605f40eae64a
cesnow030 <125354699+cesnow030@users.noreply.github.com>
2023-02-20 15:07:52 +0530

    Update python connector.py

--- python connector.py
+++ python connector.py
@@ -1,1 +1,31 @@
-import snowflake ocnnecor
+import snowflake.connector
+
+# Create a connection to Snowflake
+conn = snowflake.connector.connect(
+    user='<your_user>',
+    password='<your_password>',
+    account='<your_account>',
+    warehouse='<your_warehouse>',
+    database='<your_database>',
+    schema='<your_schema>'
+)
+
+# Create a cursor to execute SQL queries
+cur = conn.cursor()
+
+# Define your SQL query
+query = "SELECT * FROM my_table"
+
+# Execute the query
+cur.execute(query)
+
+# Fetch the results
+results = cur.fetchall()
+
+# Close the cursor and connection
+cur.close()
+conn.close()
+
+# Do something with the results
+for row in results:
+    print(row)
